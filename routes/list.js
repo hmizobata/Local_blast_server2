9ca627148556bc62aee222ee4cc5fbe6540171ae
mizobata.hideaki <mizobata.hideaki@gmail.com>
2023-01-27 12:27:57 +0900

    一応完成？

--- routes/list.js
+++ routes/list.js
@@ -4,32 +4,55 @@ var router = express.Router();
 const childprocess = require("child_process");
 const fs = require('fs');
 const { file } = require('babel-types');
+const conf = require('config');
 
-//テスト用PATH
-const dbdna = "./opt/blast/db_dna/";
-const dbprotein = "./opt/blast/db_protein/";
-const tmpplace = "./tmp/";
-const makeblastdbplace = "./ncbi-blast-2.12.0+/bin/makeblastdb.exe";
+//configファイルから変更する変数↓
 
-//suikouサーバ用PATH
-// const dbdna = "/data2/sequenceserver/sequenceserver-2.0.0-db/db_nucleotide/";
-// const dbprotein = "/data2/sequenceserver/sequenceserver-2.0.0-db/db_protein/";
-// const seqserver = "/data2/sequenceserver/sequenceserver-2.0.0-db";
-// const tmpplace = seqserver + "/tmp/";
-// const makeblastdbplace = "/suikou/tool/ncbi-blast-2.13.0+/bin/makeblastdb";
+//許容するファイルタイプ・拡張子
+const permitted_filetypes = new RegExp(conf.filetypes.permitted_filetypes_list);
+const permitted_extensions = new RegExp(conf.filetypes.permitted_extensions);
+
+//PATH
+const makeblastdbplace = conf.path.makeblastdbplace;
+const dbplace = conf.path.dbplace;
+const dbdna = conf.path.dbdna;
+const dbprotein = conf.path.dbprotein;
+const tmpplace = conf.path.tmpplace;
+const uploadplace = conf.path.uploadplace;
+const errorplace = conf.path.errorplace;
+const url_seqserver = conf.url.seqserver;
 
 //ファイル名を処理する関数。ユーザー名、ファイル名、拡張子名に分割してarrayを返す。
 function filename_split(filename){
-	//アンダーバー、".fa"でファイル名をsplit。
+	//アンダーバーでファイル名をsplit。
 	var split_underbar = filename.split("_");
-	var split_fa = filename.split(".fa");
 
-	//ユーザー名はアンダーバーでsplitしたものの第1項、拡張子は".fa"でsplitしたものの最終項+"fa"。ファイル名は全体からそれらを削ったもの。
+	//ユーザー名はアンダーバーでsplitしたものの第1項。もとのファイル名・拡張子は置換して獲得。
 	var username = split_underbar[0];
-	var extention = "fa" + split_fa[split_fa.length - 1];
-	var original_filename = filename.slice(username.length + 1).slice(0, -1-extention.length);
 
-	return [username, original_filename, extention];
+	//ファイル名からユーザー名を除去。
+	var original_filename = filename.replace(username + "_", "");
+
+	//"."でスプリット
+	var split_dot = original_filename.split(".");
+	var extension = "";
+
+	//"."でスプリットした最終項から順に見ていき、fasta等に一致したらそれ以降を拡張子とする。
+	for(let i = split_dot.length - 1; i > -1; i--){
+		if(i == split_dot.length - 1)
+			extension = split_dot[i];
+		else
+			extension = split_dot[i] + "." + extension;
+		
+		//拡張子を除去した値がoriginal_filenameである。各値が確定したらループを止める。
+		if(permitted_extensions.test(split_dot[i])){
+			var remExtension = new RegExp("." + extension + "$");
+			original_filename = original_filename.replace(remExtension,"");
+			break;
+		}
+	}
+
+	return [username, original_filename, extension];
 }
 
 //最初のページ表示。
@@ -51,7 +74,9 @@ router.get('/', (req, res, next) => {
 
 		//DB内ファイルを一つずつ取得し、ddata1にぶちこむ。最後にsort。
 		files.forEach(file => {
-			ddata1.push(file);
+			if(permitted_filetypes.test(file)){
+				ddata1.push(file);
+			}
 		});
 		ddata1.sort();
 
@@ -60,9 +85,10 @@ router.get('/', (req, res, next) => {
 		{
 			//ファイル名からユーザー名、もとのファイル名、拡張子を取得。
 			file_info = filename_split(ddata1[i]);
+			console.log(file_info);
 
 			//拡張子が.fasta,.faのものだけを集計する。
-			if(/^(fasta|fa)$/.test(file_info[2])){
+			if(permitted_extensions.test(file_info[2])){
 				duser.push(file_info[0]);
 				dnamae.push(file_info[1]);
 				dextension.push(file_info[2]);
@@ -85,7 +111,8 @@ router.get('/', (req, res, next) => {
 		
 		//DB内ファイルを一つずつ取得し、ddata1にぶちこむ。最後にsort。		
 		files.forEach(file => {
-			pdata1.push(file);
+			if(permitted_filetypes.test(file))
+				pdata1.push(file);
 		});
 		pdata1.sort();
 		
@@ -96,7 +123,7 @@ router.get('/', (req, res, next) => {
 			file_info = filename_split(pdata1[i]);
 
 			//拡張子が.fasta,.faのものだけを集計する。
-			if(/^(fasta|fa)$/.test(file_info[2])){
+			if(permitted_extensions.test(file_info[2])){
 				puser.push(file_info[0]);
 				pnamae.push(file_info[1]);
 				pextension.push(file_info[2]);
@@ -112,18 +139,12 @@ router.get('/', (req, res, next) => {
 	//ユーザー名・ファイル名・拡張子名をnucleotide/protein別に格納したデータをviews/list.pugに渡す。
 	setTimeout(function(){
 		res.render("list", {duser: ddata.duser, dnamae: ddata.dnamae, dextension: ddata.dextension,
-			 puser: pdata.puser, pnamae: pdata.pnamae, pextension: pdata.pextension});
+			 puser: pdata.puser, pnamae: pdata.pnamae, pextension: pdata.pextension, seqserver: url_seqserver});
 	}, 50);
 });
 
 //renameやremoveなど、ページ内のボタンを押した場合の処理。
 router.post('/', (req, res, next) => {
-
-	//"Upload Files"ボタンが押されたとき、uploadページに遷移して終了。
-	if(typeof(req.body.upload) != "undefined"){
-		res.redirect("upload");
-		return;
-	}
 
 	if(typeof(req.body.drenamebefore) != "undefined")//nucleotideファイルのrename
 	{
@@ -138,7 +159,8 @@ router.post('/', (req, res, next) => {
 			
 			//DB内のファイル一覧を配列にぶちこみ、ソート。
 			files.forEach(file => {
-				ddata2.push(file);
+				if(permitted_filetypes.test(file))
+					ddata2.push(file);
 			});
 			ddata2.sort();
 
@@ -149,7 +171,7 @@ router.post('/', (req, res, next) => {
 				var file_info = filename_split(ddata2[i]);
 
 				//makeblastdbでできたファイルを削除(ユーザー名&もとのファイル名がrename対象で、かつ拡張子がfasta,faじゃなければ消去)。
-				if(file_info[0]==before_info[0] && file_info[1]==before_info[1] && /^(fasta|fa)$/.test(file_info[2])==false)
+				if(file_info[0] == before_info[0] && file_info[1] == before_info[1] && permitted_extensions.test(file_info[2]) == false)
 				{
 					fs.unlinkSync(dbdna + ddata2[i]);
 				}				
@@ -162,7 +184,7 @@ router.post('/', (req, res, next) => {
 				var file_info = filename_split(ddata2[i]);
 
 				//ユーザー名・もとのファイル名・拡張子がrename対象のときに実行
-				if(file_info[0]==before_info[0] && file_info[1]==before_info[1] && /^(fasta|fa)$/.test(file_info[2]))
+				if(file_info[0] == before_info[0] && file_info[1] == before_info[1] && permitted_extensions.test(file_info[2]))
 				{
 					//rename後ファイル名を設定し、rename実行。エラーとログを出力。
 					var after = file_info[0] + "_" + req.body.drenameafter + "." + file_info[2];
@@ -191,7 +213,8 @@ router.post('/', (req, res, next) => {
 			
 			//DB内のファイル一覧を配列にぶちこみ、ソート。
 			files.forEach(file => {
-				pdata2.push(file);
+				if(permitted_filetypes.test(file))
+					pdata2.push(file);
 			});
 			pdata2.sort();
 
@@ -202,7 +225,7 @@ router.post('/', (req, res, next) => {
 				var file_info = filename_split(pdata2[i]);
 
 				//makeblastdbでできたファイルを削除(ユーザー名&もとのファイル名がrename対象で、かつ拡張子がfasta,faじゃなければ消去)。
-				if(file_info[0]==before_info[0] && file_info[1]==before_info[1] && /^(fasta|fa)$/.test(file_info[2])==false)
+				if(file_info[0] == before_info[0] && file_info[1] == before_info[1] && permitted_extensions.test(file_info[2]) == false)
 				{
 					fs.unlinkSync(dbprotein + pdata2[i]);
 				}				
@@ -215,7 +238,7 @@ router.post('/', (req, res, next) => {
 				var file_info = filename_split(pdata2[i]);
 
 				//ユーザー名・もとのファイル名・拡張子がrename対象のときに実行
-				if(file_info[0]==before_info[0] && file_info[1]==before_info[1] && /^(fasta|fa)$/.test(file_info[2]))
+				if(file_info[0] == before_info[0] && file_info[1] == before_info[1] && permitted_extensions.test(file_info[2]))
 				{
 					//rename後ファイル名を設定し、rename実行。エラーとログを出力。
 					var after = file_info[0] + "_" + req.body.prenameafter + "." + file_info[2];
@@ -237,8 +260,15 @@ router.post('/', (req, res, next) => {
 		var user = [];
 		var filename = [];
 
-		//チェックが入っているチェックボックスのファイル名を分割し、ユーザー名・ファイル名を取得。
-		req.body.dcheckbox.forEach(file => {
+		//チェックが入っているチェックボックスを取得。チェックが一つの場合はstringが返ってくるので、複数のケースに合わせてarrayに変換する。
+		var filenames;
+		if(typeof req.body.dcheckbox === "string")
+			filenames = [req.body.dcheckbox];
+		else
+			filenames = req.body.dcheckbox;
+
+		//ファイル名を分割し、ユーザー名ともとのファイル名を配列に格納。
+		filenames.forEach(file => {
 			var file_info = filename_split(file);
 			user.push(file_info[0]);
 			filename.push(file_info[1]);
@@ -254,6 +284,7 @@ router.post('/', (req, res, next) => {
 				}
 			});
 		});
+		res.redirect("list");
 	}
 	else if(typeof(req.body.pcheckbox) != "undefined" && typeof(req.body.prename == "undefined"))//proteinデータの削除
 	{
@@ -261,8 +292,15 @@ router.post('/', (req, res, next) => {
 		var user = [];
 		var filename = [];
 
-		//チェックが入っているチェックボックスのファイル名を分割し、ユーザー名・ファイル名を取得。
-		req.body.pcheckbox.forEach(file => {
+		//チェックが入っているチェックボックスを取得。チェックが一つの場合はstringが返ってくるので、複数のケースに合わせてarrayに変換する。
+		var filenames;
+		if(typeof req.body.pcheckbox === "string")
+			filenames = [req.body.pcheckbox];
+		else
+			filenames = req.body.pcheckbox;
+
+		//ファイル名を分割し、ユーザー名ともとのファイル名を配列に格納。
+		filenames.forEach(file => {
 			var file_info = filename_split(file);
 			user.push(file_info[0]);
 			filename.push(file_info[1]);
@@ -278,6 +316,7 @@ router.post('/', (req, res, next) => {
 				}
 			});
 		});
+		res.redirect("list");
 	}
 
 
--- routes/upload.js
+++ routes/upload.js
@@ -8,14 +8,23 @@ const childprocess = require("child_process");
 const { exec, execSync } = require("child_process");
 const { stdout } = require('process');
 const { callbackify } = require('util');
+const conf = require('config');
 
-//ローカルテスト用PATH
-const makeblastdbplace = "./ncbi-blast-2.12.0+/bin/makeblastdb.exe";
-const dbplace = "./opt/blast/";
-const dbdna = dbplace + "db_dna/";
-const dbprotein = dbplace + "db_protein/";
-const tmpplace = "./tmp/";
-const uploadplace = "./uploads/";
+//configファイルから変更する変数↓
+
+//許容するファイルタイプ・拡張子
+const permitted_filetypes = new RegExp(conf.filetypes.permitted_filetypes_upload);
+const permitted_extensions = new RegExp(conf.filetypes.permitted_extensions);
+
+//PATH
+const makeblastdbplace = conf.path.makeblastdbplace;
+const dbplace = conf.path.dbplace;
+const dbdna = conf.path.dbdna;
+const dbprotein = conf.path.dbprotein;
+const tmpplace = conf.path.tmpplace;
+const uploadplace = conf.path.uploadplace;
+const errorplace = conf.path.errorplace;
+const url_seqserver = conf.url.seqserver;
 
 // //suikouサーバー用PATH
 // const makeblastdbplace = "/suikou/tool/ncbi-blast-2.13.0+/bin/makeblastdb";
@@ -26,22 +35,40 @@ const uploadplace = "./uploads/";
 // const dbprotein = dbplace + "/db_protein/";
 // const uploadplace = seqserver + "/uploads/";
 
-
+//configファイルから変更する変数↑
 
 //ファイル名を処理する関数。ユーザー名、ファイル名、拡張子名に分割してarrayを返す。
 function filename_split(filename){
-	//アンダーバー、".fa"でファイル名をsplit。
+	//アンダーバーでファイル名をsplit。
 	var split_underbar = filename.split("_");
-	var split_fa = filename.split(".fa");
 
-	//ユーザー名はアンダーバーでsplitしたものの第1項、拡張子は".fa"でsplitしたものの最終項+"fa"。ファイル名は全体からそれらを削ったもの。
+	//ユーザー名はアンダーバーでsplitしたものの第1項。もとのファイル名・拡張子は置換して獲得。
 	var username = split_underbar[0];
-	var extention = "fa" + split_fa[split_fa.length - 1];
-	var original_filename = filename.slice(username.length + 1).slice(0, -1-extention.length);
 
-	return [username, original_filename, extention];
+	//ファイル名からユーザー名を除去。
+	var original_filename = filename.replace(username + "_", "");
+
+	//"."でスプリット
+	var split_dot = original_filename.split(".");
+	var extension = "";
+
+	//"."でスプリットした最終項から順に見ていき、fasta等に一致したらそれ以降を拡張子とする。
+	for(let i = split_dot.length - 1; i > -1; i--){
+		if(i == split_dot.length - 1)
+			extension = split_dot[i];
+		else
+			extension = split_dot[i] + "." + extension;
+		
+		//拡張子を除去した値がoriginal_filenameである。各値が確定したらループを止める。
+		if(permitted_extensions.test(split_dot[i])){
+			var remExtension = new RegExp("." + extension + "$");
+			original_filename = original_filename.replace(remExtension,"");
+			break;
+		}
+	}
+
+	return [username, original_filename, extension];
 }
-
 
 //ファイルを元の名前（originalname）で指定の場所(uploadplace)にアップロード。
 const storage = multer.diskStorage({
@@ -58,30 +85,24 @@ const upload = multer({storage: storage});
 
 //最初のページ表示。テンプレート(upload.pug)を読み込む。
 router.get('/', function(req, res, next) {
-  res.render("upload",);
+  res.render("upload", {seqserver: url_seqserver});
 });
 
 
 
 //「Upload」ボタンが押されたときの処理
-router.post("/", upload.any(), (req, res) =>{
-
-  if(typeof(req.body.list) != undefined){
-    res.redirect("list");
-    return;
-  }
-  
+router.post("/", upload.any(), (req, res) =>{  
   //nucleotide/protein情報、ユーザー名、ファイル名の取得
   var radio = req.body.uploadradio;
   var user = req.body.uploaduser;
   var file = req.files[0]["filename"];
 
-  //ファイルがちゃんとfasta,faになっているか、スペースを含まないか確認。不適切なファイル名の場合エラーを出力する。
-  var isFileFasta = file.match(/[.](fasta|fa)([.]gz|)$/);
+  //ファイルがちゃんとfasta,fa,fsa,fna,aaになっているか、スペースを含まないか確認。不適切なファイル名の場合エラーを出力する。
+  var isFileFasta = file.match(permitted_filetypes);
   var includeSpaces = file.match(/ /);
   
   if(isFileFasta == null)
-    return res.send("Error: This file is inappropriate for the database. <br> 1.Available extensions are '.fasta, .fa, .fasta.gz, .fa.gz'.");
+    return res.send("Error: This file is inappropriate for the database. <br> 1.Available extensions are '.fasta, .fa, .fasta.gz, .fa.gz, .fsa, .fsa.gz, .fna, .fna.gz, .aa, .aa.gz'.");
 
   if(includeSpaces != null)
     return res.send("Error: This file is inappropriate for the database. <br>Spaces are not available for the file name.");
@@ -105,18 +126,10 @@ router.post("/", upload.any(), (req, res) =>{
   for(let k = 0; k < exists.length; k++){
     //DBディレクトリ内ファイル名からユーザー名・拡張子を除いたファイル名・拡張子を取得。
     var exists_info = filename_split(exists[k]);
-    
+
     //アップロードしようとしているファイル名から、拡張子を除去した値を取得。
-    var filename = file;
-    if(/[.]fasta[.]gz$/.test(filename))
-      filename = filename.slice(0, -9);
-    else if(/[.]fa[.]gz$/.test(filename))
-      filename = filename.slice(0, -6);
-    else if(/[.]fasta$/.test(filename))
-      filename = filename.slice(0, -6);
-    else
-      filename = filename.slice(0, -3);
-    
+    var filename = file.replace(permitted_filetypes,"");
+        
     //「ユーザー名」・「拡張子を除去したファイル名」が一致する場合、エラーを出力。
     if(user==exists_info[0] && filename==exists_info[1])
       return res.send("Error: A file with the same name already exists.");
@@ -126,12 +139,11 @@ router.post("/", upload.any(), (req, res) =>{
   fs.renameSync(uplodir + file, dir + user + "_" + file);
 
   //アップロードファイルがgz形式の場合、解凍する。
-  if(/[.](fasta|fa)[.]gz$/.test(file)){
+  if(/[.]gz$/.test(file)){
     //解凍前後のファイル名をPATHごと取得。変数fileも解凍後の値に変更。
     var gunzipfile = dir + user + "_" + file;
     var gzipcontent = fs.readFileSync(gunzipfile);
     var gunzippedfile = gunzipfile.slice(0, -3);
-    file = file.slice(0, -3);
 
     //解凍処理を実行。解凍前ファイルは削除。
     zlib.gunzip(gzipcontent, function(err, data){
@@ -146,12 +158,12 @@ router.post("/", upload.any(), (req, res) =>{
   //   //削除対象ファイルのユーザー名・ファイル名・拡張子名を取得
   //   var removefile_info = filename_split(user + "_" + file);
 
-  //   //ディレクトリ内を検索し、ユーザー名・ファイル名が一致するものを削除
+  //   //ディレクトリ内を検索し、ユーザー名・ファイル名が一致するものをerrorディレクトリに移動。
   //   fs.readdir(dir2, (err, files) => {
   //     files.forEach(file => {
   //       var eachfile_info = filename_split(file);
   //       if(removefile_info[0] == eachfile_info[0] && removefile_info[1] == eachfile_info[1])
-  //         fs.unlinkSync(dir2 + file);
+  //         fs.renameSync(dir2 + file, errorplace + file);
   //     })
   //   });
 
